bf490ab241dd1d914ccc271f487597d62504c2a9
ericgaspar <junk.eg@free.fr>
2021-05-10 13:13:25 +0200

    Update config.js

--- conf/config.js
+++ conf/config.js
@@ -397,51 +397,13 @@ module.exports = {
 		//   For plain connections, use the `ldap` scheme.
 		url: "ldap://127.0.0.1",
 
-		// - `tlsOptions`: LDAP connection TLS options (only used if scheme is
-		//   `ldaps://`). It is an object whose values are Node.js' `tls.connect()`
-		//   options. It is set to `{}` by default.
-		//   For example, this option can be used in order to force the use of IPv6:
-		//   ```js
-		//   {
-		//     host: 'my::ip::v6',
-		//     servername: 'example.com'
-		//   }
-		//   ```
-		tlsOptions: {},
-
 		// - `primaryKey`: LDAP primary key. It is set to `"uid"` by default.
 		primaryKey: "uid",
 
 		// - `baseDN`: LDAP base DN, alternative to `searchDN`. For example, set it
 		//   to `"ou=accounts,dc=example,dc=com"`.
 		//   When unset, the LDAP auth logic with use `searchDN` instead to locate users.
-
-		// - `searchDN`: LDAP search DN settings. This defines the procedure by
-		//   which The Lounge first looks for the user DN before authenticating them.
-		//   It is ignored if `baseDN` is specified. It is an object with the
-		//   following keys:
-		searchDN: {
-			//   - `rootDN`: This bind DN is used to query the server for the DN of
-			//     the user. This is supposed to be a system user that has access in
-			//     read-only to the DNs of the people that are allowed to log in.
-			//     It is set to `"cn=thelounge,ou=system-users,dc=example,dc=com"` by
-			//     default.
-			rootDN: "ou=users,dc=yunohost,dc=org",
-
-			//   - `rootPassword`: Password of The Lounge LDAP system user.
-			rootPassword: "",
-
-			//   - `ldapFilter`: it is set to `"(objectClass=person)(memberOf=ou=accounts,dc=example,dc=com)"`
-			//     by default.
-			filter: "(objectClass=person)(memberOf=ou=accounts,dc=example,dc=com)",
-
-			//   - `base`: LDAP search base (search only within this node). It is set
-			//     to `"dc=example,dc=com"` by default.
-			base: "dc=yunohost,dc=org",
-
-			//   - `scope`: LDAP search scope. It is set to `"sub"` by default.
-			scope: "sub",
-		},
+		baseDN: "ou=users,dc=yunohost,dc=org",
 	},
 
 	// ## Debugging settings
